f978b6ea0ac00a996c9a10525edb7b652641c973
donald-f <donald.flobeck@gmail.com>
2025-12-07 17:05:58 -0500

    Don't let app be opened twice with mutex

--- Program.cs
+++ Program.cs
@@ -1,22 +1,42 @@
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace WallpaperCycler
 {
     internal static class Program
     {
+        private static Mutex? mutex;
+
         [STAThread]
         static void Main()
         {
+            const string mutexName = "WallpaperCycler_SingleInstanceMutex";
+
+            bool createdNew;
+            mutex = new Mutex(true, mutexName, out createdNew);
+
+            if (!createdNew)
+            {
+                // Another instance is already running
+                MessageBox.Show("WallpaperCycler is already running.", "Already Running", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
             ApplicationConfiguration.Initialize();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             Logger.Init();
             Logger.Log("Application starting");
+
             var main = new MainForm();
             Application.Run();
+
             Logger.Log("Application exiting");
+
+            // Release mutex when app exits
+            mutex.ReleaseMutex();
         }
     }
 }
